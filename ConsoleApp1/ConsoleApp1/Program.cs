38627a046d0a2cc27bd06eaf727bdb4784795b8d
student <student@DESKTOP-L3ET5DL>
2020-02-13 19:34:46 +0200

    init

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Start");
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    Console.WriteLine("Hello!");
+                    Thread.Sleep(1000);
+                }
+            }).Start();
+            Console.WriteLine("End");*/
+
+            /*Process process = new Process();
+            process.StartInfo = new ProcessStartInfo("notepad");
+            process.Start();
+
+            Thread.Sleep(5000);
+
+            process.Kill();*/
+
+            /*Process process = new Process();
+            process.StartInfo = new ProcessStartInfo("calc");
+            process.Start();
+
+            Thread.Sleep(5000);
+
+            if (!process.HasExited)
+            {
+                process.Kill();
+            }
+            else
+            {
+                process =
+                    Process.GetProcessesByName("Calculator")[0];
+                Console.WriteLine(process.Id);
+                process.Kill();
+            }*/
+
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo("CPPConsoleApp.exe");
+            process.Start();
+
+            while (!process.HasExited)
+            {
+                Thread.Sleep(500);
+            }
+
+            Console.WriteLine(process.ExitCode);
+        }
+    }
+}
